1939dcbeb0310cdf1a8c628856394af0c51dab37
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-08-01 22:19:57 -0300

    Commit final do projeto

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const host = 'http://localhost';
 const porta = 3000;
+const stats = require('./pcRunUsage.js');
 
 http.createServer((req, res) => {
-    res.end('<h1>Working</h1>')
+    let url = req.url;
+
+    if (url === '/stats'){
+        res.end(JSON.stringify(stats, null,2));
+    } else {
+        res.end('<h1>Seja bem vindo!</h1>');
+    }
+   
 }).listen(porta, () => {
-    console.log(`Server is running! ${host}:${porta}`);
+    console.log(`Server is running! ${host}:${porta}, ${stats}`);
 });
